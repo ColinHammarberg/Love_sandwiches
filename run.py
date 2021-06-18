44a88d305d089d00a8f53e65d07d8030e9bbf2cd
ColinHammarberg <colin.hammarberg2@gmail.com>
2021-06-18 22:02:29 +0000

    Added some Python

--- run.py
+++ run.py
@@ -16,8 +16,11 @@ def get_sales_data():
     """
     get sales figures input from the user 
     """
-print("Please enter sales data from the last market.")
-print("Data should be six numbers, seperated by commas.")
-print("Example: 10,20,30,40,50,60")
+    print("Please enter sales data from the last market.")
+    print("Data should be six numbers, seperated by commas.")
+    print("Example: 10,20,30,40,50,60/n")
 
-data_str = input("Enter your data here:")
+    data_str = input("Enter your data here:")
+    print(f"The data provided is {data_str}")
+
+get_sales_data()
