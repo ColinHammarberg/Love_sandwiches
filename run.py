961fbd23b72be6c94fbb834850f2e69f2865b7d9
ColinHammarberg <colin.hammarberg2@gmail.com>
2021-06-19 08:01:50 +0000

    Added some Python

--- run.py
+++ run.py
@@ -19,7 +19,7 @@ def get_sales_data():
     while True:
         print("Please enter sales data from the last market.")
         print("Data should be six numbers, seperated by commas.")
-        print("Example: 10,20,30,40,50,60/n")
+        print("Example: 10,20,30,40,50,60 \n")
 
     data_str = input("Enter your data here:")
 
@@ -41,12 +41,20 @@ def validate_data(values):
         f"Exactly six values required, you provided {len(values)}"
         )
     except ValueError as e:
-        print(f"Invalid data: {e}, Please try again./n")
+        print(f"Invalid data: {e}, Please try again. \n")
         return False
 
     return True
 
-    
+def update_sales_worksheet(data):
+    """
+    Update sales worksheet, add new row with the list data provided.
+    """
+    print("Updating sales worksheet...\n")
+    sales_worksheet = SHEET.worksheet("sales")
+    sales_worksheet.append_row(data)
+    print("Sales worksheet updated successfully. \n")
     
 
-get_sales_data()
+data = get_sales_data()
+sales_data = [int(num) for num in data]
