4299adcfe51a59c8e00ff2bcdecab8be0f647c7b
ColinHammarberg <colin.hammarberg2@gmail.com>
2021-06-19 15:58:49 +0000

    Added some Python

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -54,7 +55,26 @@ def update_sales_worksheet(data):
     sales_worksheet = SHEET.worksheet("sales")
     sales_worksheet.append_row(data)
     print("Sales worksheet updated successfully. \n")
-    
+
+def calculate_surplus_data(sales_row):
+    """
+    Compare sales with stock and calculate the surplus for each item type.
+    """
+    print("calculating surplus data...\n")
+    stock = SHEET.worksheet("stock").get_all_values()
+    stock_row = stock[-1]
+    print(stock)
+
+def main():
+
+    """
+    Run all program functions
+    """
 
 data = get_sales_data()
 sales_data = [int(num) for num in data]
+update_sales_worksheet(sales_data)
+calculate_surplus_data(sales_data)
+
+print("Welcome to Love Sandwiches Data Automation")
+main()
